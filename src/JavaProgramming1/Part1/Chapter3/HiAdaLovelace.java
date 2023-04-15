1607e1ff83856671a42e614afbdf01d8350f0a97
Marcelo Veras <marceloantonino.verass@gmail.com>
2023-04-14 23:21:46 -0300

    Added exercise from part1 chapter3

---
+++ src/JavaProgramming1/Part1/Chapter3/HiAdaLovelace.java
@@ -0,0 +1,8 @@
+package JavaProgramming1.Part1.Chapter3;
+
+public class HiAdaLovelace {
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        System.out.println("Hi " + name + "!");
+    }
+}
