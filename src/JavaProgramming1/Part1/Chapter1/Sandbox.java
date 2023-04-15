82c652b9d9dab4734a74a148ad00e39229f6c0cf
Marcelo Veras <marceloantonino.verass@gmail.com>
2023-04-14 22:59:44 -0300

    Added exercises from part1 chapter2

---
+++ src/JavaProgramming1/Part1/Chapter1/Sandbox.java
@@ -0,0 +1,7 @@
+package JavaProgramming1.Part1.Chapter1;
+
+public class Sandbox {
+    public static void main(String[] args) {
+        System.out.println("one day I will be a Senior Java Developer");
+    }
+}
---
+++ src/JavaProgramming1/Part1/Chapter2/AdaLovelace.java
@@ -0,0 +1,7 @@
+package JavaProgramming1.Part1.Chapter2;
+
+public class AdaLovelace {
+    public static void main(String[] args) {
+        System.out.println("Ada Lovelace");
+    }
+}
