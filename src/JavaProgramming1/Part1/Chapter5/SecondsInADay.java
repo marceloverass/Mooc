66d5a8beb4a3644dd95527e06776b7af92bfce9b
Marcelo Veras <marceloantonino.verass@gmail.com>
2023-04-16 20:00:54 -0300

    Added exercise from Part1 Chapter5

---
+++ src/JavaProgramming1/Part1/Chapter5/SecondsInADay.java
@@ -0,0 +1,15 @@
+package JavaProgramming1.Part1.Chapter5;
+import java.util.Scanner;
+
+public class SecondsInADay {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many days would you like to convert to seconds?");
+        int number = Integer.valueOf(scanner.nextLine());
+
+        int seconds = number * 86400;
+
+        System.out.println(seconds);
+    }
+}
