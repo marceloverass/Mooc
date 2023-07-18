ee89953892503c936e0bad2972b74ed291f28daa
Marcelo Veras <marceloantonino.verass@gmail.com>
2023-07-18 08:38:21 -0300

    Added exercise from Part1 Chapter6

---
+++ src/JavaProgramming1/Part1/Chapter6/CheckingTheAge.java
@@ -0,0 +1,16 @@
+package JavaProgramming1.Part1.Chapter6;
+import java.util.Scanner;
+public class CheckingTheAge {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How old are you?");
+        int age = Integer.valueOf(scan.nextLine());
+
+        if (age >= 0 && age <= 120) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
+    }
+}
